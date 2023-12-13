0ebe377fa11a816c6a7bd6a2d0bde8ceccbe4e2f
Vatsal Manot <vatsal.manot@yahoo.com>
2023-12-13 03:44:18 -0800

    Update package

---
+++ Sources/SwiftUIZ/Intramodular/WIP/_DynamicViewBridge.swift
@@ -0,0 +1,13 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import SwiftUIX
+
+public class _DynamicViewBridge: _HierarchicalViewBridge<_DynamicViewBridge> {
+    
+}
+
+public struct _DynamicViewDescriptor {
+    
+}
